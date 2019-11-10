54c44d160e7db69824a080015da65d53af70dfa0
Daria <daria.melinte@cultofcoders.com>
2019-11-10 21:15:35 +0200

    file storage

--- App/store/places-actions.js
+++ App/store/places-actions.js
@@ -1,11 +1,29 @@
+import * as FileSystem from 'react-native-fs';
 import Constants from '../constants/Constants';
 
 const { ADD_PLACE } = Constants;
 
-export const addPlace = (title, image) => ({
-        type: ADD_PLACE,
-        placeData: {
-            title,
-            image
-        },
-});
+export const addPlace = (title, image) => {
+    return async dispatch => {
+        const fileName = image.split('/').pop();
+        console.log(fileName);
+        const newPath = 'file://' + FileSystem.DocumentDirectoryPath + '/' + fileName;
+        // const newPath = FileSystem.DocumentDirectoryPath + fileName;
+
+        FileSystem.moveFile(image, newPath)
+        .then(() => {
+            console.log(newPath, 'newPath');
+            console.log(image, 'imagePath');
+            dispatch({
+                type: ADD_PLACE,
+                placeData: {
+                    title,
+                    image : newPath
+                },
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+}
