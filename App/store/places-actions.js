259d56bdefdb4c8a15c4347b2c2481b329ffbd43
Daria <daria.melinte@cultofcoders.com>
2019-11-11 22:07:43 +0200

    fixed image bug

--- App/store/places-actions.js
+++ App/store/places-actions.js
@@ -4,23 +4,31 @@ import Constants from '../constants/Constants';
 const { ADD_PLACE } = Constants;
 
 export const addPlace = (title, image) => {
-    return async dispatch => {
-        const fileName = image.split('/').pop();
-        console.log(fileName);
-        const newPath = 'file://' + FileSystem.DocumentDirectoryPath + '/' + fileName;
-
-        FileSystem.moveFile(image, newPath)
-        .then(() => {
+    return async dispatch => {        
+        if (image) {
+            const fileName = image.split('/').pop();
+            const newPath = 'file://' + FileSystem.DocumentDirectoryPath + '/' + fileName;
+            FileSystem.moveFile(image, newPath)
+            .then(() => {
+                dispatch({
+                    type: ADD_PLACE,
+                    placeData: {
+                        title,
+                        image : newPath
+                    },
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        } else {
             dispatch({
                 type: ADD_PLACE,
                 placeData: {
                     title,
-                    image : newPath
+                    image
                 },
             })
-        })
-        .catch(err => {
-            console.log(err);
-        })
+        }        
     }
 }
